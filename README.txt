e17f3d2276a16611543c2fafa42b4d2a51e689dc
PTParra <49121638+PTParra@users.noreply.github.com>
2024-10-15 16:46:49 +0200

    commit inicial

---
+++ README.txt
@@ -0,0 +1,3 @@
+Ejercicio de codewars sacado de:
+
+https://www.codewars.com/kata/663e0eccecb2d0a12da51f84
---
+++ app.js
@@ -0,0 +1,5 @@
+/*
+    Digitwise addition is a special kind of addition where instead of adding 1 normally to the number, 
+    it adds 1 to every digit of that number. If the digit is a 9, we replace it with a 10 without carrying over 
+    to the next digit.
+*/
